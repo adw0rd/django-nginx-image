37c9792dda0041c021f8afebc184a15f89d20736
Mikhail Andreev (adw0rd) <x11org@gmail.com>
2012-10-28 17:18:21 +0400

    Refactoring

--- nginx_image/management/commands/nginx_image_converter.py
+++ nginx_image/management/commands/nginx_image_converter.py
@@ -5,6 +5,9 @@ from optparse import make_option
 from django.core.management.base import BaseCommand
 
 
+NGX_HTTP_IMAGE_FILTER_MODULE_IS_SUPPORTS = ('JPEG', 'GIF', 'PNG', )
+
+
 class Command(BaseCommand):
     help = "Recursively the source directory, finds the BMP files and convert them to JPG.\n"\
            "Example: ./manage.py nginx_image_converter -i /storage/project/media/ -o /storage/project/newmedia -q75"
@@ -32,39 +35,43 @@ class Command(BaseCommand):
         make_option(
             '-e',
             '--change-extension',
+            action="store_true",
             dest='change_extension',
             default=False,
             help='Change extension to "jpg"'),
     )
 
     def handle(self, **options):
-        source_dir = options.get('source')
-        destination_dir = options.get('destination')
-        change_extension = options.get('change_extension')
-        quality = options.get('quality')
+        self.source_dir = options.get('source')
+        self.destination_dir = options.get('destination')
+        self.change_extension = options.get('change_extension')
+        self.quality = options.get('quality')
 
-        assert os.path.exists(source_dir)
-        assert os.path.exists(destination_dir)
+        assert os.path.exists(self.source_dir)
+        assert os.path.exists(self.destination_dir)
 
-        for source_directory, dirnames, filenames in os.walk(source_dir):
+        for source_directory, dirnames, filenames in os.walk(self.source_dir):
             for filename in filenames:
-                clean_filename, clean_ext = filename.rsplit('.', 1)
-                clean_filepath = os.sep.join([part for part in source_directory.replace(source_dir, '').split(os.sep) if part])
-
                 try:
                     image = Image.open(os.path.join(source_directory, filename).encode('utf-8'))
-                    if image.format.lower() != "bmp":
-                        continue
                 except IOError:
                     continue
+                if image.format.upper() in NGX_HTTP_IMAGE_FILTER_MODULE_IS_SUPPORTS:
+                    continue
+                self.convert(image, source_directory, filename)
 
-                if image.mode not in ('L', 'RGB'):
-                    image = image.convert('RGB')
+    def convert(self, image, source_directory, filename):
+        clean_filename, clean_ext = filename.rsplit('.', 1)
+        clean_filepath = os.sep.join([part for part in source_directory.replace(self.source_dir, '').split(os.sep) if part])
 
-                destination_directory = os.path.join(destination_dir, clean_filepath)
-                if not os.path.exists(destination_directory):
-                    os.makedirs(destination_directory)
-                extension = 'jpg' if change_extension else clean_ext
-                destination_filepath = os.path.join(destination_directory, '{}.{}'.format(clean_filename, extension))
+        if image.mode not in ('L', 'RGB'):
+            image = image.convert('RGB')
 
-                image.save(destination_filepath.encode('utf-8'), 'JPEG', quality=quality)
+        destination_directory = os.path.join(self.destination_dir, clean_filepath)
+        if not os.path.exists(destination_directory):
+            os.makedirs(destination_directory)
+
+        extension = 'jpg' if self.change_extension else clean_ext
+        destination_filepath = os.path.join(destination_directory, '{}.{}'.format(clean_filename, extension))
+
+        return image.save(destination_filepath.encode('utf-8'), 'JPEG', quality=self.quality)
